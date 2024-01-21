6a517558237d2c6ba15ff8c3d56b77f45c84418e
Boris-Elsys <boris.ya.bachev.2019@elsys-bg.org>
2024-01-21 21:14:08 +0200

    Frontend Start

---
+++ app/src/main/java/com/example/fast_pedals_frontend/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.fast_pedals_frontend
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.fast_pedals_frontend.ui.theme.FastPedalsFrontEndTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FastPedalsFrontEndTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Anddddroid")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FastPedalsFrontEndTheme {
+        Greeting("Andddroid")
+    }
+}
---
+++ app/src/main/java/com/example/fast_pedals_frontend/auth/RegisterResponse.kt
@@ -0,0 +1,6 @@
+package com.example.fast_pedals_frontend.auth
+
+data class RegisterResponse(
+    val message: String,
+    val token: String
+)
