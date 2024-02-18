74b1fa2c89c88d85245191d1aad4f9966f548dc7
BorisBachev <boris.ya.bachev.2019@elsys-bg.org>
2024-02-18 20:56:37 +0200

    Added jwt refreshing functionality

---
+++ app/src/main/java/com/example/fast_pedals_frontend/auth/start/CheckResponse.kt
@@ -0,0 +1,5 @@
+package com.example.fast_pedals_frontend.auth.start
+
+data class CheckResponse(
+    val jwt: String?
+)
--- app/src/main/java/com/example/fast_pedals_frontend/bike/ContactInfo.kt
+++
@@ -1,9 +0,0 @@
-package com.example.fast_pedals_frontend.bike
-
-data class ContactInfo(
-
-    val userId: Long,
-    val name: String,
-    val phoneNumber: String
-
-)
